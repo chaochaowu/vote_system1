7750c69537df2fe680ccb280a96300c2099f716e
chaochaowu <chaowu.dev@gmail.com>
2019-09-05 15:12:29 +0900

    add delete candidate feature

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -29,6 +29,12 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy if @candidate
+    redirect_to candidates_path, notice: '候選人資料已刪除！'
+  end
+
   private
 
   def candidate_params
