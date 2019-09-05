8dde811630679ea1485675a2a960c45572e037b8
chaochaowu <chaowu.dev@gmail.com>
2019-09-05 16:20:50 +0900

    1. plus VoteLog modle 2. modify vote feature 3. add vote_log_count column to count votes

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,6 @@
 class CandidatesController < ApplicationController
+  before_action :find_candidate, only: [:edit, :update, :destroy, :vote]
+
   def index
     @candidates = Candidate.all  # instance variable can use in views
   end
@@ -17,11 +19,9 @@ class CandidatesController < ApplicationController
   end
 
   def edit
-    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def update
-    @candidate = Candidate.find_by(id: params[:id])
     if @candidate.update(candidate_params)
       redirect_to candidates_path, notice: '更新候選人成功！'
     else
@@ -30,14 +30,22 @@ class CandidatesController < ApplicationController
   end
 
   def destroy
-    @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy if @candidate
     redirect_to candidates_path, notice: '候選人資料已刪除！'
+  end
+
+  def vote
+    @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
+    redirect_to candidates_path, notice: '完成投票！'
   end
 
   private
 
   def candidate_params
     params.require(:candidate).permit(:name, :age,:party, :politics)
+  end
+
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_111651) do
+ActiveRecord::Schema.define(version: 2019_09_05_065936) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_09_04_111651) do
     t.integer "votes", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "vote_logs_count"
+  end
+
+  create_table "vote_logs", force: :cascade do |t|
+    t.integer "candidate_id"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["candidate_id"], name: "index_vote_logs_on_candidate_id"
   end
 
 end
